05ac2fcc0256208b4e8d5ca8142e8aeaf76b702d
Oleg Efimov <efimovov@gmail.com>
2011-06-11 16:47:03 +0400

    Add API search method implementation with tests

--- test/api.js
+++ test/api.js
@@ -4,9 +4,11 @@
  * See license text in LICENSE file
  */
 
+var Async = require('async');
+
 var Wikimapia = require('../lib/wikimapia');
 
-exports.New = function (test) {
+exports.TestNew = function (test) {
   test.expect(1);
 
   var api = new Wikimapia();
@@ -16,14 +18,18 @@ exports.New = function (test) {
   test.done();
 };
 
-exports.BadKey = function (test) {
+exports.TestBadKey = function (test) {
   test.expect(1);
 
   var api = new Wikimapia({
     key: ''
   });
 
-  api.getObject(55, function (err, json) {
+  api.object(55, function (err, json) {
+    if (err) {
+      throw err;
+    }
+
     test.same(json, {
       debug: [ {
         code: 1000,
@@ -33,4 +39,67 @@ exports.BadKey = function (test) {
     
     test.done();
   });
+};
+
+exports.Object = function (test) {
+  test.expect(3);
+
+  var api = new Wikimapia();
+  var objectId = 55;
+  var objectTitle = 'Eiffel Tower';
+  var objectCountry = 'France';
+
+  api.object(objectId, function (err, json) {
+    if (err) {
+      throw err;
+    }
+
+    test.equals(json.id, objectId);
+
+    test.equals(json.title, objectTitle);
+
+    test.equals(json.location.country, objectCountry);
+
+    test.done();
+  });
+};
+
+exports.Search = function (test) {
+  test.expect(1);
+
+  var api = new Wikimapia();
+  var objectId = 55;
+  var objectTitle = 'Eiffel Tower';
+  var objectCountry = 'France';
+
+  var founded = false;
+  var page = 1;
+  
+  Async.until(
+    function () {
+      return founded || (page > 5);
+    },
+    function (cb) {
+      api.search(objectTitle, page, function (err, json) {
+        if (err) {
+          throw err;
+        }
+
+        founded = json.folder.some(function (object) {
+          return (object.id == objectId) && (object.name == objectTitle) && (object.location.country == objectCountry);
+        });
+
+        page++;
+
+        cb();
+      });
+    },
+    function (err) {
+      if (err) {
+        throw err;
+      }
+      test.ok(founded);
+      test.done();
+    }
+  );
 };
